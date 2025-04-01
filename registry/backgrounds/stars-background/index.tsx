9f38d303017d9c0a854146fdfae9bf4a9296e502
Elliot Sutton <elsk8@outlook.fr>
2025-04-01 21:12:15 +0200

    chore: add .vscode settings and upgrade registry

--- registry/backgrounds/stars-background/index.tsx
+++ registry/backgrounds/stars-background/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { motion, SpringOptions, useMotionValue, useSpring } from 'motion/react';
 import * as React from 'react';
+import { motion, SpringOptions, useMotionValue, useSpring } from 'motion/react';
 
 import { cn } from '@/lib/utils';
 
