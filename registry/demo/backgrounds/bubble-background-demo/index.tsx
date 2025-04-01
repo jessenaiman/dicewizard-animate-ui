75380d6d6e94bab2be32cefb156096805ca0a811
Pierre-Eric Marchandet <119690799+pem-dialonce@users.noreply.github.com>
2025-04-01 08:44:58 +0200

    Update Bubble background demo

--- registry/demo/backgrounds/bubble-background-demo/index.tsx
+++ registry/demo/backgrounds/bubble-background-demo/index.tsx
@@ -1,13 +1,13 @@
 import { BubbleBackground } from '@/registry/backgrounds/bubble-background';
 
-export const GradientBackgroundDemo = () => {
+export const BubbleBackgroundDemo = () => {
   return (
     <BubbleBackground
       interactive
       className="absolute inset-0 flex items-center justify-center rounded-xl"
     >
       <h1 className="text-3xl font-bold text-white z-10">
-        Gradient Background
+        Bubble Background
       </h1>
     </BubbleBackground>
   );
