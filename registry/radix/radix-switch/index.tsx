c9de4823d45c915839122f403d01d0742c5f1923
Elliot Sutton <elsk8@outlook.fr>
2025-04-07 19:48:13 +0200

    fix: radix switch focus outline

--- registry/radix/radix-switch/index.tsx
+++ registry/radix/radix-switch/index.tsx
@@ -51,7 +51,7 @@ const Switch = React.forwardRef<
         <motion.button
           ref={ref}
           className={cn(
-            'relative flex p-[3px] h-6 w-10 shrink-0 cursor-pointer items-center rounded-full transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input data-[state=checked]:justify-end data-[state=unchecked]:justify-start',
+            'relative flex p-[3px] h-6 w-10 shrink-0 cursor-pointer items-center rounded-full transition-colors focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input data-[state=checked]:justify-end data-[state=unchecked]:justify-start',
             className,
           )}
           whileTap="tap"
