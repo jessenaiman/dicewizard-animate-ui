0cca45c75794d96f44cb548da0848acb137d9dfb
Elliot Sutton <elsk8@outlook.fr>
2025-04-10 22:39:06 +0200

    feat: improve tabs component animation

--- components/animate-ui/tabs.tsx
+++ components/animate-ui/tabs.tsx
@@ -1,12 +1,7 @@
 'use client';
 
 import * as React from 'react';
-import {
-  motion,
-  type Tween,
-  type Transition,
-  useAnimation,
-} from 'motion/react';
+import { motion, type Transition } from 'motion/react';
 
 import { cn } from '@/lib/utils';
 import {
