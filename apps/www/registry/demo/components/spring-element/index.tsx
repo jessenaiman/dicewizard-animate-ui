37d981e8b3dd3ac7c704b335b3c1ab33c26b44c8
Mohammed Arham Khan <md.arhamkhan09@gmail.com>
2025-06-01 15:32:40 +0530

    feat: add spring element component

---
+++ apps/www/registry/demo/components/spring-element/index.tsx
@@ -0,0 +1,20 @@
+import { SpringElement } from '@/registry/components/spring-element';
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from '@workspace/ui/components/ui/avatar';
+
+export default function SpringElementDemo() {
+  return (
+    <SpringElement>
+      <Avatar className="size-12">
+        <AvatarImage
+          draggable={false}
+          src="https://pbs.twimg.com/profile_images/1897311929028255744/otxpL-ke_400x400.jpg"
+        />
+        <AvatarFallback>AK</AvatarFallback>
+      </Avatar>
+    </SpringElement>
+  );
+}
