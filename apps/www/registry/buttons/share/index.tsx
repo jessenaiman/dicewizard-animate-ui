cc12bc1b8750b75964ddb4c5ea2c381d49746f66
momotom <a18520145493@163.com>
2025-06-27 14:08:07 +0800

    feat: realize button content animation switching

--- apps/www/registry/buttons/share/index.tsx
+++ apps/www/registry/buttons/share/index.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { Share2, Github, X, Facebook } from 'lucide-react';
 import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@workspace/ui/lib/utils';
-import { HTMLMotionProps, motion } from 'motion/react';
+import { HTMLMotionProps, motion, AnimatePresence } from 'motion/react';
 
 const buttonVariants = cva(
   "relative overflow-hidden cursor-pointer inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-lg text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
@@ -13,13 +13,13 @@ const buttonVariants = cva(
         default: 'bg-primary text-primary-foreground hover:bg-primary/90',
       },
       size: {
-        default: 'h-10 px-4 py-2 has-[>svg]:px-3',
-        sm: 'h-9 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
-        lg: 'h-11 px-8 has-[>svg]:px-6',
+        default: 'min-w-28 h-10 px-4 py-2',
+        sm: 'min-w-24 h-9 rounded-md gap-1.5 px-3',
+        lg: 'min-w-32 h-11 px-8',
       },
       icon: {
-        suffix: 'pl-4 pr-12',
-        prefix: 'pl-12 pr-4',
+        suffix: 'pl-4',
+        prefix: 'pr-4',
       },
     },
     defaultVariants: {
@@ -28,6 +28,13 @@ const buttonVariants = cva(
     },
   },
 );
+
+const iconSizeMap = {
+  sm: 16,
+  md: 20,
+  lg: 28,
+  default: 16,
+};
 
 type ShareButtonProps = HTMLMotionProps<'button'> & {
   children: React.ReactNode;
@@ -41,19 +48,49 @@ function ShareButton({
   children,
   className,
   variant,
-  size,
+  size = 'default',
   icon,
   ...props
 }: ShareButtonProps) {
+  const [hovered, setHovered] = React.useState(false);
   return (
     <motion.button
       className={cn(buttonVariants({ variant, size, className, icon }))}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
       {...props}
     >
-      {/* {icon === 'prefix' && <Share2 className="size-4" />}
-      {children}
-      {icon === 'suffix' && <Share2 className="size-4" />} */}
-      <ShareIconGroup size={size} />
+      <AnimatePresence initial={false} mode="wait">
+        {!hovered ? (
+          <motion.div
+            key="content"
+            initial={{ opacity: 1, y: 0 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -24 }}
+            transition={{ duration: 0.3 }}
+            className=" absolute left-0 right-0 top-0 bottom-0 flex items-center justify-center gap-2"
+          >
+            {icon === 'prefix' && (
+              <Share2 className="size-4" size={iconSizeMap[size]} />
+            )}
+            {children}
+            {icon === 'suffix' && (
+              <Share2 className="size-4" size={iconSizeMap[size]} />
+            )}
+          </motion.div>
+        ) : (
+          <motion.div
+            key="icons"
+            initial={{ opacity: 0, y: 24 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 24 }}
+            transition={{ duration: 0.3 }}
+            className=" absolute left-0 right-0 top-0 bottom-0 flex items-center justify-center gap-2"
+          >
+            <ShareIconGroup size={size} />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </motion.button>
   );
 }
@@ -78,6 +115,7 @@ type ShareIconGroupProps = {
 } & VariantProps<typeof shareIconGroupVariants>;
 
 function ShareIconGroup({ size = 'default', className }: ShareIconGroupProps) {
+  const iconSize = iconSizeMap[size];
   return (
     <motion.div className={cn(shareIconGroupVariants({ size }), className)}>
       <motion.div
@@ -85,21 +123,21 @@ function ShareIconGroup({ size = 'default', className }: ShareIconGroupProps) {
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0, duration: 0.5, type: 'spring', bounce: 0.4 }}
       >
-        <Github size={size} />
+        <Github size={iconSize} />
       </motion.div>
       <motion.div
         initial={{ opacity: 0, y: 24 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.1, duration: 0.5, type: 'spring', bounce: 0.4 }}
       >
-        <X size={size} />
+        <X size={iconSize} />
       </motion.div>
       <motion.div
         initial={{ opacity: 0, y: 24 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.2, duration: 0.5, type: 'spring', bounce: 0.4 }}
       >
-        <Facebook size={size} />
+        <Facebook size={iconSize} />
       </motion.div>
     </motion.div>
   );
