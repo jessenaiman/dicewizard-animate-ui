d40f8c900d4a496203ccb8c69e51522e259ae851
Elliot Sutton <elsk8@outlook.fr>
2025-08-11 10:18:57 +0200

    feat: v1

--- apps/www/registry/demo/primitives/animate/tooltip/index.tsx
+++ apps/www/registry/demo/primitives/animate/tooltip/index.tsx
@@ -5,7 +5,6 @@ import {
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-  TooltipPortal,
 } from '@/registry/primitives/animate/tooltip';
 
 export const AnimateTooltipDemo = () => {
@@ -13,52 +12,44 @@ export const AnimateTooltipDemo = () => {
     <TooltipProvider>
       <div className="flex flex-col gap-5 justify-center items-center">
         <div className="flex flex-row gap-2 border p-2">
-          <Tooltip placement="top">
+          <Tooltip>
             <TooltipTrigger className="bg-accent px-4 py-2">
               Docs
             </TooltipTrigger>
 
-            <TooltipPortal>
-              <TooltipContent className="bg-primary px-3 py-1.5 text-sm text-primary-foreground">
-                <p>Documentation</p>
-              </TooltipContent>
-            </TooltipPortal>
+            <TooltipContent className="bg-primary px-3 py-1.5 text-sm text-primary-foreground">
+              <p>Documentation</p>
+            </TooltipContent>
           </Tooltip>
 
-          <Tooltip placement="top">
+          <Tooltip>
             <TooltipTrigger className="bg-accent px-4 py-2">API</TooltipTrigger>
 
-            <TooltipPortal>
-              <TooltipContent className="bg-primary px-3 py-1.5 text-sm text-primary-foreground">
-                <p>API Reference</p>
-              </TooltipContent>
-            </TooltipPortal>
+            <TooltipContent className="bg-primary px-3 py-1.5 text-sm text-primary-foreground">
+              <p>API Reference</p>
+            </TooltipContent>
           </Tooltip>
 
-          <Tooltip placement="top">
+          <Tooltip>
             <TooltipTrigger className="bg-accent px-4 py-2">
               Guide
             </TooltipTrigger>
 
-            <TooltipPortal>
-              <TooltipContent className="bg-primary px-3 py-1.5 text-sm text-primary-foreground">
-                <p>User Guide</p>
-              </TooltipContent>
-            </TooltipPortal>
+            <TooltipContent className="bg-primary px-3 py-1.5 text-sm text-primary-foreground">
+              <p>User Guide</p>
+            </TooltipContent>
           </Tooltip>
         </div>
         <div className="flex flex-row gap-5">
-          <Tooltip placement="top">
+          <Tooltip>
             <TooltipTrigger className="bg-accent px-4 py-2">
               Repo
             </TooltipTrigger>
 
-            <TooltipPortal>
-              <TooltipContent className="bg-primary px-3 py-1.5 text-sm text-primary-foreground">
-                {/* <TooltipArrow className="bg-primary size-2.5 rotate-45" /> */}
-                <p>GitHub</p>
-              </TooltipContent>
-            </TooltipPortal>
+            <TooltipContent className="bg-primary px-3 py-1.5 text-sm text-primary-foreground">
+              {/* <TooltipArrow className="bg-primary size-2.5 rotate-45" /> */}
+              <p>GitHub</p>
+            </TooltipContent>
           </Tooltip>
         </div>
       </div>
