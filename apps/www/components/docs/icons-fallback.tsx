c175146e199492b46d2c135623344b30dbfb12d2
Elliot Sutton <elsk8@outlook.fr>
2025-09-06 19:26:15 +0200

    feat: update the changelog

---
+++ apps/www/components/docs/icons-fallback.tsx
@@ -0,0 +1,17 @@
+import { Skeleton } from '@workspace/ui/components/ui/skeleton';
+
+export const IconsFallback = () => {
+  return (
+    <div className="-mt-4.5 space-y-4 text-black dark:text-white">
+      <Skeleton className="h-4 w-38" />
+
+      <Skeleton className="h-10 w-full" />
+
+      <div className="w-full grid lg:grid-cols-11 sm:grid-cols-9 xs:grid-cols-7 grid-cols-5 gap-4 mt-6">
+        {new Array(99).fill(0).map((_, index) => (
+          <Skeleton className="size-full aspect-square" key={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
