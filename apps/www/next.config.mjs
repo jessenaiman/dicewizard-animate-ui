9ea729cc7cbb9f8b25d20c96b65421883d64b354
Elliot Sutton <elsk8@outlook.fr>
2025-09-08 23:41:41 +0200

    fix: redirects

--- apps/www/next.config.mjs
+++ apps/www/next.config.mjs
@@ -18,8 +18,68 @@ const config = {
   async redirects() {
     return [
       {
-        source: '/docs/components/:path*',
-        destination: '/docs/components/animate/:path*',
+        source: '/docs/components/avatar-group',
+        destination: '/docs/components/animate/avatar-group',
+        permanent: true,
+      },
+      {
+        source: '/docs/components/code-editor',
+        destination: '/docs/components/animate/code',
+        permanent: true,
+      },
+      {
+        source: '/docs/components/code-tabs',
+        destination: '/docs/components/animate/code-tabs',
+        permanent: true,
+      },
+      {
+        source: '/docs/components/counter',
+        destination: '/docs/components/animate/counter',
+        permanent: true,
+      },
+      {
+        source: '/docs/components/cursor',
+        destination: '/docs/components/animate/cursor',
+        permanent: true,
+      },
+      {
+        source: '/docs/components/files',
+        destination: '/docs/components/radix/files',
+        permanent: true,
+      },
+      {
+        source: '/docs/components/motion-grid',
+        destination: '/docs/primitives/animate/motion-grid',
+        permanent: true,
+      },
+      {
+        source: '/docs/components/pinned-list',
+        destination: '/docs/primitives/animate/pinned-list',
+        permanent: true,
+      },
+      {
+        source: '/docs/components/scroll-progress',
+        destination: '/docs/primitives/animate/scroll-progress',
+        permanent: true,
+      },
+      {
+        source: '/docs/components/spring-element',
+        destination: '/docs/primitives/animate/spring',
+        permanent: true,
+      },
+      {
+        source: '/docs/components/stars-scrolling-wheel',
+        destination: '/docs/components/animate/github-stars-wheel',
+        permanent: true,
+      },
+      {
+        source: '/docs/components/tabs',
+        destination: '/docs/components/animate/tabs',
+        permanent: true,
+      },
+      {
+        source: '/docs/components/tooltip',
+        destination: '/docs/components/animate/tooltip',
         permanent: true,
       },
       {
