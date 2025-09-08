24f6cf30963b4ed5a5bc661fb5dc26f38a5a1e74
Elliot Sutton <elsk8@outlook.fr>
2025-09-08 23:28:05 +0200

    chore: add redirects (for SEO)

--- apps/www/next.config.mjs
+++ apps/www/next.config.mjs
@@ -15,6 +15,60 @@ const config = {
     ],
   },
   reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: '/docs/components/:path*',
+        destination: '/docs/components/animate/:path*',
+        permanent: true,
+      },
+      {
+        source: '/docs/radix/:path*',
+        destination: '/docs/components/radix/:path*',
+        permanent: true,
+      },
+      {
+        source: '/docs/base/:path*',
+        destination: '/docs/components/radix/:path*',
+        permanent: true,
+      },
+      {
+        source: '/docs/headless/:path*',
+        destination: '/docs/components/radix/:path*',
+        permanent: true,
+      },
+      {
+        source: '/docs/buttons/:path*',
+        destination: '/docs/primitives/buttons/:path*',
+        permanent: true,
+      },
+      {
+        source: '/docs/backgrounds/:path*',
+        destination: '/docs/components/backgrounds/:path*',
+        permanent: true,
+      },
+      {
+        source: '/docs/effects/motion-effect',
+        destination: '/docs/primitives/effects/effect',
+        permanent: true,
+      },
+      {
+        source: '/docs/effects/motion-highlight',
+        destination: '/docs/primitives/effects/highlight',
+        permanent: true,
+      },
+      {
+        source: '/docs/effects/:path*',
+        destination: '/docs/primitives/effects/:path*',
+        permanent: true,
+      },
+      {
+        source: '/docs/ui-elements/:path*',
+        destination: '/docs/components/community/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withMDX(config);
