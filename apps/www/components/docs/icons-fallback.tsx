90bbec840b14350463100c9b4d3af7ac80702d85
Elliot Sutton <elsk8@outlook.fr>
2025-09-13 19:03:29 +0200

    feat: add new icons

--- apps/www/components/docs/icons-fallback.tsx
+++ apps/www/components/docs/icons-fallback.tsx
@@ -7,6 +7,11 @@ export const IconsFallback = () => {
 
       <Skeleton className="h-10 w-full" />
 
+      <div className="flex items-center gap-2">
+        <Skeleton className="h-7 w-10 rounded-full" />
+        <Skeleton className="h-7 w-12 rounded-full" />
+      </div>
+
       <div className="w-full grid lg:grid-cols-11 sm:grid-cols-9 xs:grid-cols-7 grid-cols-5 gap-4 mt-6">
         {new Array(99).fill(0).map((_, index) => (
           <Skeleton className="size-full aspect-square" key={index} />
